d996a16ac0d0ec7d11a7580a7b9e42b5825d8e18
Sara Alkhamri <s.alkhamri@gmail.com>
2020-02-19 18:03:11 -0800

    Added Server.js

---
+++ server-jessica/Server.js
@@ -0,0 +1,32 @@
+const express = require('express') // require the express npm module, needs to be added to the project using "yarn add" or "npm install"
+const helmet = require('helmet'); //third party middleware for configuring security headers in Node
+const cors = require('cors'); //third party middleware to make it easy to configure CORS in Node
+
+
+//import routes
+// const authRouter = require('./auth/auth-router')
+// const usersRouter = require('./users/users-router')
+
+const server = express(); //creates an express application using the express module
+
+
+//call to .use() to tell express which middleware to use. Must come AFTER the server has been created by calling express().
+server.use(helmet()); //tells express to use helmet middleware
+server.use(express.json()); //built-in middleware in express
+server.use(cors()); //tells express to use cors middleware
+
+
+//routes go here
+// server.use('/api/auth', authRouter);
+// server.use('/api/users', usersRouter);
+
+
+//configures our server to excute a function for ever GET request to '/'
+//the second argument passed to the .get() method is the 'Route Handler function', which will run on every GET request to '/'
+server.get('/', (req, res) => { 
+    //express passes the request and response objects to this function
+    //the .send on the response object can be used to send a response to the client
+    res.send('Server up!')
+})
+
+module.exports = server;
