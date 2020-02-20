bdf86efdc61c86ab21b56aa4bb0bba81f1e92c52
Sara Alkhamri <s.alkhamri@gmail.com>
2020-02-19 19:00:05 -0800

    Added schema for users table

--- database/migrations/20200219184321_users.js
+++ database/migrations/20200219184321_users.js
@@ -1,8 +1,19 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable('users', tbl => {
+        tbl.increments(); //creates a primary key called id
+
+        tbl.string('username', 128) //creates string field that is both required and unique
+        .unique()
+        .notNullable()
+
+        tbl.string('passowrd', 128) //creates a passowrd field that is required
+        .notNullable()
+    })
   
 };
 
 exports.down = function(knex) {
-  
+    //drops entire table, which allows to undo any changes made to the schema is necessary.
+    return knex.schema.dropTableIfExists('users');
 };
