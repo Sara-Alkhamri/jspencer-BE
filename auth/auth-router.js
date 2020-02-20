6aad22b67582a1d915ed94a27a2ad96b040da3f3
Sara Alkhamri <s.alkhamri@gmail.com>
2020-02-20 15:22:28 -0800

    Added comments to auth-router.js

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -27,17 +27,39 @@ router.post('/register', (req, res) => {
   //user login
   //Endpoint auth/login
 
+router.post('/login', (req, res) => {
+    let { username, password } = req.body;
+  
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        //check that passwords match
+        if (user && bcrypt.compareSync(password, user.password)) {
+          const token = generateToken(user);
+        //server returns the token to the client
+          res.status(200).json({
+            message: `Welcome ${user.username}!, I have a token`,
+            token, //attach token as part of the response
+          });
+        } else {
+          res.status(401).json({ message: "Invalid Credentials" });
+        }
+    })
+      .catch(error => {
+        res.status(500).json(error);
+    });
+});  
 
   function generateToken(user) {
     const payload = {
-      subject: user.id,
+      subject: user.id, //subject in the token is what the token is about
       username: user.username,
     };
   
     const options = {
       expiresIn: "4d"
     };
-  
-    return jwt.sign(payload, secrets.jwtSecret, options);
+  //extract the secret away so it can be required and used where needed
+    return jwt.sign(payload, secrets.jwtSecret, options); //this method is synchronous
 }
 module.exports = router;  
