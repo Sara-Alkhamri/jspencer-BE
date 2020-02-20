c5c7f17af2187a09366f6c74eee8d0c7fcc78144
Sara Alkhamri <s.alkhamri@gmail.com>
2020-02-20 13:34:56 -0800

    Added restricted middleware

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+//secret
+
+module.exports = (req, res, next) => {
+    const token = req.headers.authorization;
+    // console.log('MIDDLEWARE')
+    if(token) {
+      //check that token is valid
+      jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+        if(err) {
+          //foul play
+          console.log(err)
+          res.status(401).json({ message: 'Invalid Credentials' });
+        } else {
+          //token is good
+          // req.user = {username: decodedToken.username};
+          req.decodedJwt = decodedToken;
+          console.log("decoded token", req.decodedJwt);
+          next();
+        }
+      })
+    } else {
+      res.status(400).json({ message: "no credentials provided" })
+    }
+  };  
