4cc1e36abe9cb4a023ae37bfd95be104cc4dfdd3
Sara Alkhamri <s.alkhamri@gmail.com>
2020-02-19 18:16:08 -0800

    Deleted server-jessica directory for better file visibility

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const server = require('./server');
+const server = require('./Server');
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 6000;
 server.listen(port, () => { //server listening for connections to the port
                             //the callback function passed as the second argument will run once when the server starts
     console.log(`Server up on ${port}`);
