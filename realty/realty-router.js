53121b735f80071cf43d9bef090443f731fe160b
Sara Alkhamri <s.alkhamri@gmail.com>
2020-02-29 18:33:58 -0800

    Added a get all photos endpoint in realty-router.js

---
+++ realty/realty-router.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const realty = require('./realty-model');
+
+const restricted = require('../auth/restricted-middleware');
+
+//get all photos
+router.get('/', (req, res) => {
+    realty.get()
+    .then(image => {
+        res.status(200).json(image);
+    })
+    .catch(err => {
+        res.status(500).json({message: 'An error occured'})
+    })
+})
