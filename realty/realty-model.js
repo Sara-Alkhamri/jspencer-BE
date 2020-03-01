9cfce3e1a53861a5c295e85599df8f05ab467a98
Sara Alkhamri <s.alkhamri@gmail.com>
2020-02-29 17:41:21 -0800

    Realty model completed

--- realty/realty-model.js
+++ realty/realty-model.js
@@ -1,1 +1,27 @@
 const db = require('../database/dbConfig');
+
+module.exports = {
+    add,
+    find,
+    findBy,
+    findById,
+};
+
+async function add(image) {
+    const [id] = await db('image').insert(image, 'id');
+    return findById(id);
+}
+
+function find() {
+    return db('image').select('id', 'image')
+}
+
+function findBy(filter) {
+    return db('image').where(filter)
+}
+
+function findById(id) {
+    return db('image')
+        .where({id})
+        .first();
+}
