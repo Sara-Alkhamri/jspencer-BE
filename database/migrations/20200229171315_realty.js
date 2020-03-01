8e8aeeb73f6a763a7957a12250f05e21849fc0fe
Sara Alkhamri <s.alkhamri@gmail.com>
2020-02-29 17:27:06 -0800

    Added realty table info

--- database/migrations/20200229171315_realty.js
+++ database/migrations/20200229171315_realty.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable('realty', tbl => {
+        tbl.increments(); //primary key
+
+        tbl.string('image', 500)
+        .notNullable()
+        .unique();
+    })
   
 };
 
 exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('image');
   
 };
