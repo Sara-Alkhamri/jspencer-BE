5a73f6120928c63b1458d524a72624c00e718cc0
Sara Alkhamri <s.alkhamri@gmail.com>
2020-03-08 21:06:47 -0700

    Contact table migrated suuccessfully

--- database/migrations/20200308205858_contact.js
+++ database/migrations/20200308205858_contact.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable('contact', tbl => {
+        tbl.increments()
+        tbl.string('firstName', 128).notNullable()
+        tbl.string('lastName', 128).notNullable();
+        tbl.string('email').notNullable();
+        tbl.text('message', 1000);
+    })
   
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('contact');
 };
