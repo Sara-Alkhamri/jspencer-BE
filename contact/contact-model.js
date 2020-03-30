b474e2bf557e63b8320abad8ff316adc591f41ce
Sara Alkhamri <s.alkhamri@gmail.com>
2020-03-30 16:22:34 -0700

    Updates to contact-model.js

--- contact/contact-model.js
+++ contact/contact-model.js
@@ -3,19 +3,35 @@ const db = require('../database/dbConfig');
 module.exports = {
     add,
     find,
+    findById,
     remove
 }
 
 //methods to handel CRUD operations for contact table
 
-async function add(message) {
-    const [id] = await db('contact').insert(message, 'id');
-    return find(id);
-  }
+// async function add(message) {
+//     const [id] = await db('contact').insert(message, 'id');
+//     return findById(id);
+//   }
+
+function add(msg) {
+    return db('contact')
+    .insert(msg, 'id')
+    .then(ids => {
+        return findById(ids[0]);
+    })
+}
 
 function find() {
     return db('contact').select('id', 'firstName', 'lastName', 'email', 'message')
 }
+
+function findById(id) {
+    return db('contact')
+        .where({id})
+        .first()
+}
+
 
 function remove(id) {
     return db('contact')
